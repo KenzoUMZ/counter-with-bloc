8e4c321c2b2045d5745214d2d522ae018235c4b8
Samuel Kenzo Umezawa <kenzoumezawa@gmail.com>
2025-02-11 10:28:37 -0300

    feat: implement Dio for photo fetching

--- lib/pages/album/album_page.dart
+++ lib/pages/album/album_page.dart
@@ -1,99 +1,96 @@
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:gap/gap.dart';
 
-Future<List<Photo>> fetchPhotos(http.Client client) async{
-  final response = await client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+import 'dio/repository_dio.dart';
 
-  return compute(parsePhotos, response.body);
-}
-
-List<Photo> parsePhotos(String responseBody){
-  final parsed = (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
-
-  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
-}
-
-class Photo{
-  final int albumId;
-  final int id;
-  final String title;
-  final String url;
-  final String thumbnailUrl;
-  const Photo({
-    required this.albumId,
-    required this.id,
-    required this.title,
-    required this.url,
-    required this.thumbnailUrl,
-  });
-
-  factory Photo.fromJson(Map<String, dynamic> json){
-    return Photo(
-      albumId: json['albumId'] as int,
-      id: json['id'] as int,
-      title: json['title'] as String,
-      url: json['url'] as String,
-      thumbnailUrl: json['thumbnailUrl'] as String,
-    );
-  }
-}
-
-class AlbumPage extends StatefulWidget{
+class AlbumPage extends StatefulWidget {
   const AlbumPage({super.key});
 
   @override
-  State<AlbumPage> createState() => _AlbumPage();
+  State<AlbumPage> createState() => _AlbumPageState();
 }
 
-class _AlbumPage extends State<AlbumPage>{
-  late Future<List<Photo>> futurePhotos;
-
-  @override
-  void initState(){
-    super.initState();
-    futurePhotos = fetchPhotos(http.Client());
-  }
-  
+class _AlbumPageState extends State<AlbumPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder<List<Photo>>(
-        future: futurePhotos,
+        future: fetchPhotos(),
         builder: (context, snapshot) {
-          if(snapshot. hasError){
-            return const Center(
-              child: Text('Anerror has ocurred!'),
+          if (snapshot.connectionState == ConnectionState.done) {
+            var data = snapshot.data;
+            return ListView.separated(
+              itemCount: data?.length ?? 0,
+              separatorBuilder: (context, index) => Gap(10),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(data![index].title),
+                  subtitle: Text(data[index].url),
+                );
+              },
             );
-          }else if(snapshot.hasData){
-            return PhotoList(photos: snapshot.data!);
-          }else{
+          } else {
             return const Center(
               child: CircularProgressIndicator(),
             );
           }
-        }
+        },
       ),
     );
   }
 }
 
-class PhotoList extends StatelessWidget {
-  const PhotoList({super.key, required this.photos});
-  final List<Photo> photos;
+// class AlbumPage extends StatefulWidget {
+//   const AlbumPage({super.key});
 
-  @override
-  Widget build(BuildContext context){
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      itemCount: photos.length,
-      itemBuilder: (context, index){
-        return Text(photos[index].title);
-      },
-    );
-  }
-}
+//   @override
+//   State<AlbumPage> createState() => _AlbumPage();
+// }
+
+// class _AlbumPage extends State<AlbumPage> {
+//   late Future<List<Photo>> futurePhotos = fetchPhotos();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: FutureBuilder<List<Photo>>(
+//           future: futurePhotos,
+//           builder: (context, snapshot) {
+//             if (snapshot.hasError) {
+//               return const Center(
+//                 child: Text('Anerror has ocurred!'),
+//               );
+//             } else if (snapshot.hasData) {
+//               return PhotoList(photos: snapshot.data!);
+//             } else {
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             }
+//           }),
+//     );
+//   }
+// }
+
+// class PhotoList extends StatelessWidget {
+//   const PhotoList({super.key, required this.photos});
+//   final List<Photo> photos;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.builder(
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 2,
+//       ),
+//       itemCount: photos.length,
+//       itemBuilder: (context, index) {
+//         return Text(photos[index].title);
+//       },
+//     );
+//   }
+// }
--- lib/pages/album/dio/repository_dio.dart
+++ lib/pages/album/dio/repository_dio.dart
@@ -1,32 +1,22 @@
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
 
-uptadeAlbum() async{
+Future<List<Photo>> fetchPhotos() async {
   final dio = Dio();
-  var response = await dio.get('https://jsonplaceholder.typicode.com/photos');
+  Response response =
+      await dio.get('https://jsonplaceholder.typicode.com/photos');
 
+  return (response.data as List).map((photo) {
+    return Photo.fromJson(photo);
+  }).toList();
 }
 
-Future<List<Photo>> fetchPhotos() async{
-  final response = await client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-
-  return compute(parsePhotos, response.body);
-}
-
-List<Photo> parsePhotos(String responseBody){
-  final parsed = (jsonDecode(responseBody) as List).cast<Map<String, dynamic>>();
-
-  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
-}
-
-class Photo{
+class Photo {
   final int albumId;
   final int id;
   final String title;
   final String url;
   final String thumbnailUrl;
+
   const Photo({
     required this.albumId,
     required this.id,
@@ -35,7 +25,7 @@ class Photo{
     required this.thumbnailUrl,
   });
 
-  factory Photo.fromJson(Map<String, dynamic> json){
+  factory Photo.fromJson(Map<String, dynamic> json) {
     return Photo(
       albumId: json['albumId'] as int,
       id: json['id'] as int,
