8c474cf4fc5b1fecde3cf3123f4145013a305941
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-02-13 15:30:44 -0300

    feat: UiCard

--- lib/pages/album/album_page.dart
+++ lib/pages/album/album_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../domain/entities/photo.dart';
 import '../album_details/album_details_page.dart';
 import 'cubit/album_cubit.dart';
 import 'cubit/album_state.dart';
@@ -41,41 +42,65 @@ class _AlbumPageState extends State<AlbumPage> {
             itemCount: state.photos.length,
             separatorBuilder: (_, index) => const Gap(10),
             itemBuilder: (_, index) {
-              return Center(
-                child: Card(
-                  clipBehavior: Clip.hardEdge,
-                  child: InkWell(
-                      splashColor: Colors.white,
-                      onTap: () {},
-                      child: SizedBox(
-                        width: 300,
-                        height: 100,
-                        child: ListTile(
-                          title: Text(
-                            state.photos[index].title ?? '',
-                            style: GoogleFonts.lato(),
-                          ),
-                          subtitle: Text(
-                            state.photos[index].url ?? '',
-                            style: GoogleFonts.abel(),
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => AlbumDetails(
-                                          photoId: state.photos[index].id ?? 0,
-                                )
-                              )
-                            );
-                          },
-                        ),
-                      )),
-                ),
+              Photo photo = state.photos[index];
+              return UiCard(
+                title: photo.title ?? '',
+                url: photo.url ?? '',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AlbumDetails(
+                        photoId: photo.id ?? 0,
+                      ),
+                    ),
+                  );
+                },
               );
             },
           );
         },
+      ),
+    );
+  }
+}
+
+class UiCard extends StatelessWidget {
+  final String title;
+  final String url;
+  final VoidCallback onTap;
+  const UiCard({
+    super.key,
+    required this.title,
+    required this.url,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+            splashColor: Colors.white,
+            onTap: () {},
+            child: SizedBox(
+              width: 300,
+              height: 100,
+              child: ListTile(
+                title: Text(
+                  title,
+                  style: GoogleFonts.lato(),
+                ),
+                subtitle: Text(
+                  url,
+                  style: GoogleFonts.abel(),
+                ),
+                onTap: () {
+                  onTap();
+                },
+              ),
+            )),
       ),
     );
   }
